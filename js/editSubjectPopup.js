9661091fd9ce0d1eeee1b5abf24240d16a7dc550
MarcoPauli <marco.pauli@web.de>
2023-06-09 11:44:04 +0200

    testlöschung2

--- js/editSubjectPopup.js
+++ js/editSubjectPopup.js
@@ -9,7 +9,7 @@ function showEditSubjectsPopup(THIS) {
 }
 
 function saveSubjectValue(x) {
-    editSubjectValue = x.id;
+    editSubjectValue = x.parentElement.id;
     return editSubjectValue;
 }
 
@@ -31,10 +31,10 @@ function renameSubject() {
     checkID(newSubjectName.value);
     if ((IDisEqual == false) && (newSubjectName.value.trim() != "")) {
         let subject = document.getElementById(editSubjectValue);
-        subject.parentNode.id = newSubjectName.value;
+        subject.children[0].id = "sub" + newSubjectName.value;
         subject.removeAttribute("id");
-        subject.setAttribute("id", "sub" + newSubjectName.value);
-        subject.innerHTML = newSubjectName.value;
+        subject.setAttribute("id", newSubjectName.value);
+        subject.children[0].innerHTML = newSubjectName.value;
         showInformation('Fach in "' + newSubjectName.value + '" umbenannt!', "green");
     } else {
         showInformation("Bitte anderen Namen überlegen!", "red");
@@ -66,17 +66,17 @@ function checkToDeleteSubject() {
     let deleteThisSubject = document.getElementById("deleteThisSubject");
     let deleteThisSubjectNot = document.getElementById("deleteThisSubjectNot");
 
-    if (deleteThisSubject.checked && (subject.id != null)) {
+    if (deleteThisSubject.checked && (subject != null)) {
         deleteSubject(subject);
         deleteThisSubjectNot.checked = "checked";
-        showInformation('Fach "' + subject.parentElement.children[0].innerHTML + '" erfolgreich gelöscht', 'green')
+        showInformation('Fach "' + subject.children[0].innerHTML + '" erfolgreich gelöscht', 'green')
     } else if (subject == null) {
         showInformation("Fach wurde schon gelöscht", "red");
     }
 }
 
 function deleteSubject(subject) {
-    subject.parentElement.remove();
+    subject.remove();
 }
 
 //This function shows - if executed - the given information
